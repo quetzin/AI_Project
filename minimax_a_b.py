fe706532a4b1dcf42a900a4e4b3eb81c407fcd20
EriMidorikawa <124942220+EriMidorikawa@users.noreply.github.com>
2023-10-26 21:13:36 -0500

    base code

--- minimax_a_b.py
+++ minimax_a_b.py
@@ -1,53 +1,53 @@
-def minimax_ab(position, depth, player, use_thresh, pass_thresh):
-    def deep_enough(position, depth):
-        # Implement your termination condition here.
-        return depth == 0  # Change this condition as needed.
+# Define your evaluation function STATIC here for the game state.
+def STATIC(position, player):
+    # Implement your evaluation function here.
+    pass
 
-    def static(position, player):
-        # Implement your evaluation function here.
-        pass
+# Define your MOVE-GEN function for generating possible moves.
+def MOVE_GEN(position, player):
+    # Implement MOVE-GEN function here.
+    pass
 
-    def move_gen(position, player):
-        # Implement MOVE-GEN function here.
-        pass
+# Define OPPOSITE function to switch between players.
+def OPPOSITE(player):
+    # Implement OPPOSITE function here.
+    pass
 
-    def opposite(player):
-        # Implement the OPPOSITE function here.
-        pass
+class SearchResult:
+    def __init__(self, value, path):
+        self.value = value
+        self.path = path
 
-    def evaluate_direction(board, player, dx, dy):
-        # Implement your evaluation for a specific direction (horizontal, vertical, diagonal).
-        pass
+def DEEP_ENOUGH(position, depth):
+    # Implement DEEP_ENOUGH function here.
+    return depth == 0  # Change this condition as needed.
 
-    def alpha_beta(position, depth, player, use_thresh, pass_thresh):
-        if deep_enough(position, depth):
-            return {'value': static(position, player), 'path': []}
+def MINIMAX_AB(position, depth, player, passThresh, useThresh):
+    if DEEP_ENOUGH(position, depth):
+        return SearchResult(STATIC(position, player), [])
 
-        successors = move_gen(position, player)
+    successors = MOVE_GEN(position, player)
 
-        if not successors:
-            return {'value': static(position, player), 'path': []}
+    if not successors:
+        return SearchResult(STATIC(position, player), [])
 
-        value = float('-inf')
-        path = []
+    value = float('-inf')
+    path = []
+    for succ in successors:
+        resultSucc = MINIMAX_AB(succ, depth + 1, OPPOSITE(player), -passThresh, -useThresh)
+        newValue = -resultSucc.value
 
-        for succ in successors:
-            result_succ = alpha_beta(succ, depth + 1, opposite(player), -pass_thresh, -use_thresh)
-            new_value = -result_succ['value']
+        if newValue > passThresh:
+            passThresh = newValue
+            path = resultSucc.path.copy()
+            path.insert(0, succ)
 
-            if new_value > pass_thresh:
-                pass_thresh = new_value
-                path = [succ] + result_succ['path']
+        if passThresh >= useThresh:
+            return SearchResult(passThresh, path)
 
-            if pass_thresh >= use_thresh:
-                return {'value': pass_thresh, 'path': path}
+        if newValue > value:
+            value = newValue
+            path = resultSucc.path.copy()
+            path.insert(0, succ)
 
-            if new_value > value:
-                value = new_value
-                path = [succ] + result_succ['path']
-
-        return {'value': pass_thresh, 'path': path}
-
-    result = alpha_beta(position, depth, player, float('-inf'), float('inf'))
-
-    return result
+    return SearchResult(passThresh, path)
