4b5b93a51c33f96d343c2b962c420d541d2ef99a
EriMidorikawa <124942220+EriMidorikawa@users.noreply.github.com>
2023-10-26 21:08:16 -0500

    added base code

--- minimax_a_b.py
+++ minimax_a_b.py
@@ -1,4 +1,53 @@
-from sys import exit
-print('hello world')
-exit()
+def minimax_ab(position, depth, player, use_thresh, pass_thresh):
+    def deep_enough(position, depth):
+        # Implement your termination condition here.
+        return depth == 0  # Change this condition as needed.
 
+    def static(position, player):
+        # Implement your evaluation function here.
+        pass
+
+    def move_gen(position, player):
+        # Implement MOVE-GEN function here.
+        pass
+
+    def opposite(player):
+        # Implement the OPPOSITE function here.
+        pass
+
+    def evaluate_direction(board, player, dx, dy):
+        # Implement your evaluation for a specific direction (horizontal, vertical, diagonal).
+        pass
+
+    def alpha_beta(position, depth, player, use_thresh, pass_thresh):
+        if deep_enough(position, depth):
+            return {'value': static(position, player), 'path': []}
+
+        successors = move_gen(position, player)
+
+        if not successors:
+            return {'value': static(position, player), 'path': []}
+
+        value = float('-inf')
+        path = []
+
+        for succ in successors:
+            result_succ = alpha_beta(succ, depth + 1, opposite(player), -pass_thresh, -use_thresh)
+            new_value = -result_succ['value']
+
+            if new_value > pass_thresh:
+                pass_thresh = new_value
+                path = [succ] + result_succ['path']
+
+            if pass_thresh >= use_thresh:
+                return {'value': pass_thresh, 'path': path}
+
+            if new_value > value:
+                value = new_value
+                path = [succ] + result_succ['path']
+
+        return {'value': pass_thresh, 'path': path}
+
+    result = alpha_beta(position, depth, player, float('-inf'), float('inf'))
+
+    return result
